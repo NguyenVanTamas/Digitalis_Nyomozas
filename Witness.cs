b3b5e46ae048c90990df7e3194da7571987b406d
Nguyen Van Tamás <nguyen.van.tamas@petrik.hu>
2026-01-29 14:43:04 +0100

    Witness class complete

---
+++ Witness.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Digitalis_Nyomozas
+{
+	internal class Witness
+	{
+		private Person person;
+		private string testimony;
+		private string testimony_date;
+
+		public Witness(Person person, string testimony, string testimony_date)
+		{
+			this.person = person;
+			this.testimony = testimony;
+			this.testimony_date = testimony_date;
+		}
+
+		public string Testimony { get => testimony; set => testimony = value; }
+		public string Testimony_date { get => testimony_date; set => testimony_date = value; }
+		internal Person Person { get => person; set => person = value; }
+	}
+}
