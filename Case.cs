f6963c34ea0543e1547b938d8c218d8d58e6f1a3
Nguyen Van Tamás <nguyen.van.tamas@petrik.hu>
2026-01-29 14:30:21 +0100

    Case class complete

---
+++ Case.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Digitalis_Nyomozas
+{
+	internal class Case
+	{
+		private int id;
+		private string title;
+		private string description;
+		private string status;
+		private List<Case> persons;
+		private List<Case> evidences;
+
+		public Case(int id, string title, string description, string status, List<Case> persons, List<Case> evidences)
+		{
+			this.id = id;
+			this.title = title;
+			this.description = description;
+			this.status = status;
+			this.persons = persons;
+			this.evidences = evidences;
+		}
+
+		public int Id { get => id; set => id = value; }
+		public string Title { get => title; set => title = value; }
+		public string Description { get => description; set => description = value; }
+		public string Status { get => status; set => status = value; }
+		internal List<Case> Persons { get => persons; set => persons = value; }
+		internal List<Case> Evidences { get => evidences; set => evidences = value; }
+	}
+}
