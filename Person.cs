0c4ec98a045fa55dc779c27aaa0bd52985622699
Nguyen Van Tamás <nguyen.van.tamas@petrik.hu>
2026-01-29 14:37:44 +0100

    Person class complete

---
+++ Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Digitalis_Nyomozas
+{
+	internal class Person
+	{
+		private string name;
+		private int age;
+		private string addendum;
+
+		public Person(string name, int age, string addendum)
+		{
+			this.name = name;
+			this.age = age;
+			this.addendum = addendum;
+		}
+
+		public string Name { get => name; set => name = value; }
+		public int Age { get => age; set => age = value; }
+		public string Addendum { get => addendum; set => addendum = value; }
+	}
+}
