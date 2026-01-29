6e7f47749eb3c4d18cc77867d538e990c671483a
Nguyen Van Tamás <nguyen.van.tamas@petrik.hu>
2026-01-29 14:49:12 +0100

    TimelineEvent class complete

---
+++ TimelineEvent.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Digitalis_Nyomozas
+{
+	internal class TimelineEvent
+	{
+		private DateTime date;
+		private string description;
+
+		public TimelineEvent(DateTime date, string description)
+		{
+			this.date = date;
+			this.description = description;
+		}
+
+		public DateTime Date { get => date; set => date = value; }
+		public string Description { get => description; set => description = value; }
+	}
+}
