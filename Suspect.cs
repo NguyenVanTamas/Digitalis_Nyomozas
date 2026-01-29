8548d0064dd805d9a0a7abb750d1db664b1ec60b
Nguyen Van Tamás <nguyen.van.tamas@petrik.hu>
2026-01-29 14:41:13 +0100

    Suspect class complete

---
+++ Suspect.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Digitalis_Nyomozas
+{
+	internal class Suspect
+	{
+		private Person person;
+		private int suspicity;
+		private string status;
+
+		public Suspect(Person person, int suspicity, string status)
+		{
+			this.person = person;
+			this.suspicity = suspicity;
+			this.status = status;
+		}
+
+		public int Suspicity { get => suspicity; set => suspicity = value; }
+		public string Status { get => status; set => status = value; }
+		internal Person Person { get => person; set => person = value; }
+	}
+}
